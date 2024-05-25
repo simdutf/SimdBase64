172c089bd609e923296bf46260fa66849b7eeeae
Nick Nuon <myjpntrip@gmail.com>
2024-05-25 19:10:39 -0400

    added base64 tables + isasciiwhitespace

--- src/Base64.cs
+++ src/Base64.cs
@@ -8,6 +8,22 @@ namespace SimdUnicode
 {
     public static class Base64
     {
+        // public static bool IsAsciiWhiteSpace<T>(T c) where T : IComparable, IComparable<char>
+        // {
+        //     return c.CompareTo(' ') == 0 || c.CompareTo('\t') == 0 || c.CompareTo('\n') == 0 || c.CompareTo('\r') == 0 || c.CompareTo('\f') == 0;
+        // }
+
+        public static bool IsAsciiWhiteSpace(char c)
+        {
+            return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\f';
+        }
+
+        public enum Base64Options
+        {
+            Base64Default = 0, // Standard base64 format
+            Base64Url = 1      // Base64url format
+        }
+
 
     }
 }

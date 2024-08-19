7ad337486e8e7984e49d2cc575f4ddc846600462
Nick Nuon <myjpntrip@gmail.com>
2024-08-18 22:30:29 -0400

    TruncatedCharErrorUTF16SSE pass

--- src/Base64SSEUTF16.cs
+++ src/Base64SSEUTF16.cs
@@ -189,6 +189,22 @@ namespace SimdBase64
                         int lastBlockSrcCount = 0;
                         while ((bufferPtr - startOfBuffer) % 64 != 0 && src < srcEnd)
                         {
+                            if (!IsValidBase64Index(*src))
+                            {
+                                bytesConsumed = Math.Max(0, (int)(src - srcInit) - lastBlockSrcCount - (int)bufferBytesConsumed);
+                                bytesWritten = Math.Max(0, (int)(dst - dstInit) - (int)bufferBytesWritten);
+
+                                int remainderBytesConsumed = 0;
+                                int remainderBytesWritten = 0;
+
+                                OperationStatus result =
+                                    Base64WithWhiteSpaceToBinaryScalar(source.Slice(Math.Max(0, bytesConsumed)), dest.Slice(Math.Max(0, bytesWritten)), out remainderBytesConsumed, out remainderBytesWritten, isUrl);
+
+                                bytesConsumed += remainderBytesConsumed;
+                                bytesWritten += remainderBytesWritten;
+                                return result;
+                            }
+
                             byte val = toBase64[(int)*src];
                             *bufferPtr = val;
                             if (val > 64)
@@ -260,6 +276,15 @@ namespace SimdBase64
 
                             while (leftover < 4 && src < srcEnd)
                             {
+
+                                if (!IsValidBase64Index(*src))
+                                {
+                                    bytesConsumed = (int)(src - srcInit);
+                                    bytesWritten = (int)(dst - dstInit);
+                                    return OperationStatus.InvalidData;
+                                }
+
+
                                 byte val = toBase64[(byte)*src];
                                 if (val > 64)
                                 {

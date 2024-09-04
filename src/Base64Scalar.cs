2046ea988ba9f9c5d08a5f913dbf69ce0297f679
Daniel Lemire <dlemire@lemire.me>
2024-09-03 23:02:55 -0400

    more inlining

--- src/Base64Scalar.cs
+++ src/Base64Scalar.cs
@@ -8,7 +8,6 @@ namespace SimdBase64
 {
     namespace Scalar
     {
-
         public static class Base64
         {
             public enum Endianness
@@ -46,7 +45,7 @@ namespace SimdBase64
                 return length / 4 * 3 + (length % 4) - 1;
             }
 
-
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public unsafe static OperationStatus DecodeFromBase64Scalar(ReadOnlySpan<byte> source, Span<byte> dest, out int bytesConsumed, out int bytesWritten, bool isUrl = false)
             {
 
@@ -189,13 +188,13 @@ namespace SimdBase64
                 }
             }
 
-
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static bool IsValidBase64Index(char b)
             {
                 return b < 256;
             }
 
-
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public unsafe static OperationStatus DecodeFromBase64Scalar(ReadOnlySpan<char> source, Span<byte> dest, out int bytesConsumed, out int bytesWritten, bool isUrl = false)
             {
                 byte[] toBase64 = isUrl != false ? Tables.ToBase64UrlValue : Tables.ToBase64Value;
@@ -347,6 +346,7 @@ namespace SimdBase64
             }
 
             // like DecodeFromBase64Scalar, but it will not write past the end of the ouput buffer.
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public unsafe static OperationStatus SafeDecodeFromBase64Scalar(ReadOnlySpan<byte> source, Span<byte> dest, out int bytesConsumed, out int bytesWritten, bool isUrl = false)
             {
 
@@ -520,11 +520,10 @@ namespace SimdBase64
                 }
             }
 
-
             // like DecodeFromBase64Scalar, but it will not write past the end of the ouput buffer.
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public unsafe static OperationStatus SafeDecodeFromBase64Scalar(ReadOnlySpan<char> source, Span<byte> dest, out int bytesConsumed, out int bytesWritten, bool isUrl = false)
             {
-
                 byte[] toBase64 = isUrl != false ? Tables.ToBase64UrlValue : Tables.ToBase64Value;
                 uint[] d0 = isUrl != false ? Base64Url.d0 : Base64Default.d0;
                 uint[] d1 = isUrl != false ? Base64Url.d1 : Base64Default.d1;
@@ -758,6 +757,7 @@ namespace SimdBase64
                 return r;
             }
 
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static OperationStatus Base64WithWhiteSpaceToBinaryScalar(ReadOnlySpan<char> input, Span<byte> output, out int bytesConsumed, out int bytesWritten, bool isUrl = false)
             {
                 int length = input.Length;
@@ -820,8 +820,7 @@ namespace SimdBase64
                 return r;
             }
 
-
-
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static int Base64LengthFromBinary(int length, bool isUrl = false)
             {
                 if (isUrl)
@@ -832,6 +831,7 @@ namespace SimdBase64
                 return (length + 2) / 3 * 4;
             }
 
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public unsafe static OperationStatus SafeBase64ToBinaryWithWhiteSpace(ReadOnlySpan<byte> input, Span<byte> output, out int bytesConsumed, out int bytesWritten, bool isUrl = false)
             {
                 // The implementation could be nicer, but we expect that most times, the user
@@ -931,6 +931,7 @@ namespace SimdBase64
                 return r;
             }
 
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public unsafe static OperationStatus SafeBase64ToBinaryWithWhiteSpace(ReadOnlySpan<char> input, Span<byte> output, out int bytesConsumed, out int bytesWritten, bool isUrl = false)
             {
                 // The implementation could be nicer, but we expect that most times, the user
@@ -1029,11 +1030,7 @@ namespace SimdBase64
                 bytesWritten += tailBytesWritten;
                 return r;
             }
-
-
-
         }
     }
-
 }
 
